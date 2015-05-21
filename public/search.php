11f9eb97c4381b25b143f6bb904c74e5d4666441
Wun Chiou <wun@utdallas.edu>
2015-05-21 14:21:00 -0500

    Search works

--- public/search.php
+++ public/search.php
@@ -24,6 +24,7 @@ $config = [
 	'account_suffix' => '',
 	'domain_controllers' => [getenv('LDAP_DC')],
 	'base_dn' => getenv('LDAP_DN'),
+	'person_filter' => 'objectClass=person',
 	'user_id_key' => getenv('LDAP_USERKEY'),
 	'admin_username' => getenv('LDAP_USER'),
 	'admin_password' => getenv('LDAP_PASS')
@@ -38,7 +39,7 @@ $config = [
 try {
     $ad = new Adldap($config);
 
-    echo "Connected to LDAP.";
+    //echo "Connected to LDAP.";
 } catch(AdldapException $e) {
     echo "Uh oh, looks like we had an issue trying to connect: $e";
 }
@@ -49,10 +50,33 @@ try {
  */
 
 $results = 'not yet implemented';
-
+$results = $ad->search()
+	->where('displayname','=',$displayname)
+	->select(['displayname','department','title','mail'])
+	->get();
 
 /**
  * Return response
  */
-echo "<p>You searched for {$displayname}.</p>";
-echo "<p>Results: {$results}</p>";
+?>
+<table>
+	<caption>Search results for <?php echo $displayname; ?></caption>
+	<thead>
+		<tr>
+			<th>Display Name</th>
+			<th>Department</th>
+			<th>Title</th>
+			<th>Email</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($results as $result) { ?>
+		<tr>
+			<td><?php if (array_key_exists('displayname',$result)) echo $result['displayname']; ?></td>
+			<td><?php if (array_key_exists('department',$result)) echo $result['department']; ?></td>
+			<td><?php if (array_key_exists('title',$result)) echo $result['title']; ?></td>
+			<td><?php if (array_key_exists('mail',$result)) echo $result['mail']; ?></td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
